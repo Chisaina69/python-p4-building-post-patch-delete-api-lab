85e90048acca21a6b7c17f9581398f66aee985b1
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-09-25 01:10:18 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
@@ -18,53 +17,50 @@ db.init_app(app)
 def home():
     return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
 
-@app.route('/bakeries')
-def bakeries():
+# Route to create a new baked good
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form
+    name = data.get('name')
+    price = float(data.get('price'))  # Convert price to float
+    # Create a new baked good in the database
+    new_baked_good = BakedGood(name=name, price=price)
+    db.session.add(new_baked_good)
+    db.session.commit()
+    # Return the newly created baked good as JSON
+    return jsonify(new_baked_good.to_dict()), 201  # 201 Created status code
 
-    bakeries = Bakery.query.all()
-    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
+# Route to update the name of a bakery
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery_name(id):
+    data = request.form
+    new_name = data.get('name')
+    # Find the bakery by its ID
+    bakery = Bakery.query.get(id)
+    if bakery is None:
+        return jsonify({'error': 'Bakery not found'}), 404
+    # Update the bakery's name if a new name is provided
+    if new_name:
+        bakery.name = new_name
+        db.session.commit()
+    # Return the updated bakery as JSON
+    return jsonify(bakery.to_dict())
 
-    response = make_response(
-        bakeries_serialized,
-        200
-    )
-    return response
+# Route to delete a baked good by its ID
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    # Find the baked good by its ID
+    baked_good = BakedGood.query.get(id)
+    if baked_good is None:
+        return jsonify({'error': 'Baked good not found'}), 404
+    # Delete the baked good from the database
+    db.session.delete(baked_good)
+    db.session.commit()
+    # Return a success message
+    return jsonify({'message': 'Baked good deleted'})
 
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-
-    bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-
-    response = make_response(
-        bakery_serialized,
-        200
-    )
-    return response
-
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
-    baked_goods_by_price_serialized = [
-        bg.to_dict() for bg in baked_goods_by_price
-    ]
-    
-    response = make_response(
-        baked_goods_by_price_serialized,
-        200
-    )
-    return response
-
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
-    most_expensive_serialized = most_expensive.to_dict()
-
-    response = make_response(
-        most_expensive_serialized,
-        200
-    )
-    return response
+# Rest of your routes (bakeries, bakery_by_id, baked_goods_by_price, most_expensive_baked_good) remain unchanged
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
